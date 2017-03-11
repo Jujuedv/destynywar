84b63049ea300f66b0e17cf1c24a6567d301f0d9
TurnTheTideTM <richard.viehoever@tum.de>
2017-03-11 03:07:51 +0100

    Fix a typo

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,2 @@
 from app.models.counter import *
+from app.models.user import *
--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,6 @@
 from app import db
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True)
