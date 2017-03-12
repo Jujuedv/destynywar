4a61c9ffef5ee3bced2b4ed446c4871052c0d458
Jujuedv <jujuedv@gmail.com>
2017-03-12 18:06:46 +0100

    added planets

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,11 +1,17 @@
 from datetime import datetime
 
+from math import cos, sin
+
+import math
+
 from app import db
+from app.models import Globalvars
+
 
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
-    points = db.Column(db.Integer, default=0) #TODO what are the actual starting points??
+    points = db.Column(db.Integer, default=0)  # TODO what are the actual starting points??
     x = db.Column(db.Integer)
     y = db.Column(db.Integer)
 
@@ -44,10 +50,19 @@ class Planet(db.Model):
     __table_args__ = (db.UniqueConstraint('x', 'y', name='__coord_uc'),
                       )
 
-    def __init__(self, owner = None, name = "Alienplanet"):
-        #TODO calculate x, y
+    def __init__(self, owner=None, name="Alienplanet"):
+        # coord calculation just taken from old version, yes radians and degrees are wrongly mixed here
+        vars = Globalvars.get()
+        if vars.planet_rot >= 360:
+            vars.planet_rot -= 360
+            vars.planet_dist += 1
+
+        self.x = round(cos(vars.planet_rot) * vars.planet_dist)
+        self.y = round(sin(vars.planet_rot) * vars.planet_dist)
+        vars.planet_rot += 360 / ((vars.planet_dist + 1) * math.pi) * 4
+
         self.name = name
         self.owner = owner
 
     def __repr__(self):
-        return "<Planet {}: {}>".format(self.id, self.name)
+        return "<Planet {}({}|{}): {}>".format(self.id, self.x, self.y, self.name)
