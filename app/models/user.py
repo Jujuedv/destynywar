fa9fef1880409a3ad2f621b9185c9ca499a2df55
Daniel Eberhard <eberhardd@iabg.de>
2017-03-17 16:33:01 +0100

    unmerged

--- app/models/user.py
+++ app/models/user.py
@@ -3,8 +3,6 @@ from sqlalchemy.exc import IntegrityError
 
 from app import db
 from app.models.holomail import Holomail
-from app.models.alliance import Alliance, alliance_association_table
-from passlib.hash import pbkdf2_sha256
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,9 +10,6 @@ class User(db.Model):
     email = db.Column(db.String(128), index=True)
     password = db.Column(db.String(128))
     planets = db.relationship('Planet', back_populates='owner')
-    alliance_member = db.relationship("Alliance", back_populates="members", lazy='dynamic', secondary=alliance_association_table)
-
-    alliance_owner = db.relationship("Alliance", backref="owner", lazy="dynamic", foreign_keys="Alliance.owner_id")
     mails_sent = db.relationship("Holomail", backref="sender", lazy="dynamic", foreign_keys="Holomail.sender_id")
     mails_received = db.relationship("Holomail", backref="receiver", lazy="dynamic",
                                      foreign_keys="Holomail.receiver_id")
